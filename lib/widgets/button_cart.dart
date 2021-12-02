85dca067e25a05d4de046182057f1b2b780f9203
Mochamad Fauzan Osama <66822622+zan-os@users.noreply.github.com>
2021-12-02 22:31:39 +0700

    feat: membuat widget cart button
    button yang menampilkan total jumlah dan harga produk yang ditambahkan ke cart dan berfungsi sebagai navigasi ke halaman cart

---
+++ lib/widgets/button_cart.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, use_key_in_widget_constructors
+
+import 'package:capstone/common/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class CartButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+            primary: onPrimary, onPrimary: Colors.white),
+        child: Container(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              RichText(
+                text: TextSpan(
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    WidgetSpan(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 4.0),
+                        child: Icon(CupertinoIcons.cart_fill, size: 18),
+                      ),
+                    ),
+                    TextSpan(text: '8 items')
+                  ],
+                ),
+              ),
+              Text('Total: \$890.00')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
