ca1adf2189141c0475073ee87873d247370fc75b
Shelly Tansil <shellytansil93@gmail.com>
2021-12-05 19:45:37 +0700

    fix: memperbaiki bug pada onPressed button

--- lib/widgets/rounded_button.dart
+++ lib/widgets/rounded_button.dart
@@ -11,7 +11,7 @@ class RoundedButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () => onClick,
+      onPressed: () => onClick(),
       child: Text(text),
       style: ElevatedButton.styleFrom(
         minimumSize: const Size.fromHeight(56.0),
