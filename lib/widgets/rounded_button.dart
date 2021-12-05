eb6f1d5ea9ca1bbc7b6c00d9dc1087eee2cc9836
Shelly Tansil <shellytansil93@gmail.com>
2021-12-05 14:39:26 +0700

    feat: membuat widget rounded button

---
+++ lib/widgets/rounded_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RoundedButton extends StatelessWidget {
+  final Function onClick;
+  final String text;
+
+  const RoundedButton({Key? key, required this.onClick, required this.text})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => onClick,
+      child: Text(text),
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size.fromHeight(56.0),
+        primary: Colors.blueAccent,
+        onPrimary: Colors.white,
+        textStyle: const TextStyle(fontSize: 16.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+      ),
+    );
+  }
+}
