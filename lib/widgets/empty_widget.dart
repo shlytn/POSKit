d17bd8d678d956f96e590260c33df2a714d6380a
Shelly Tansil <shellytansil93@gmail.com>
2021-12-07 14:01:29 +0700

    feat: membuat widget untuk no data

---
+++ lib/widgets/empty_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+
+class EmptyWidget extends StatelessWidget {
+  const EmptyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery
+        .of(context)
+        .size
+        .width * 0.5;
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Image.asset('images/search.png', width: size),
+          const Text('No Data'),
+        ],
+      ),
+    );
+  }
+}
