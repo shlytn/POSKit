8bc4824e07ccb54a970c71413d19245823edb209
Shelly Tansil <shellytansil93@gmail.com>
2021-12-07 14:10:46 +0700

    feat: menambahkan halaman Home (temporary)

---
+++ lib/ui/stat_page.dart
@@ -0,0 +1,12 @@
+import 'package:dicoding_capstone_pos/widgets/empty_widget.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class StatPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: EmptyWidget(),
+    );
+  }
+}
