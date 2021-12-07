c8018e3f20a6ab942ae981d91de3a282e6e50e0d
Shelly Tansil <shellytansil93@gmail.com>
2021-12-07 14:13:50 +0700

    feat: menambahkan halaman Settings (temporary)

---
+++ lib/ui/settings_page.dart
@@ -0,0 +1,15 @@
+import 'package:dicoding_capstone_pos/widgets/empty_widget.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  static const routeName = '/profile';
+  static const pageTitle = 'Profile';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: EmptyWidget(),
+    );
+  }
+}
