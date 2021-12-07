0fad66cfbebdac5339f40028afd94977addb9b2e
Shelly Tansil <shellytansil93@gmail.com>
2021-12-07 14:13:16 +0700

    feat: menambahkan halaman History (temporary)

---
+++ lib/ui/history_page.dart
@@ -0,0 +1,15 @@
+import 'package:dicoding_capstone_pos/widgets/empty_widget.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HistoryPage extends StatelessWidget {
+  static const routeName = '/history';
+  static const pageTitle = 'History';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: EmptyWidget(),
+    );
+  }
+}
