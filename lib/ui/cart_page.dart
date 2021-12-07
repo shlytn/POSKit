a240b30756c6bcf9f2b61ea524db59da2ec77a5e
Fauzan <osamafauzan33@gmail.com>
2021-12-07 22:47:54 +0700

    feat: menambahkan halaman cart

---
+++ lib/ui/cart_page.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:dicoding_capstone_pos/widgets/cart_button.dart';
+import 'package:dicoding_capstone_pos/widgets/list_cart.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CartPage extends StatefulWidget {
+  static const routeName = '/cart';
+  const CartPage({Key? key}) : super(key: key);
+
+  @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  bool isListView = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        title: Text('Cart'),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 8.0),
+          Expanded(
+            child: ListViewCart(),
+          ),
+          CartButton(
+            route: CartPage.routeName,
+          ),
+          SizedBox(height: 8.0)
+        ],
+      ),
+    );
+  }
+}
