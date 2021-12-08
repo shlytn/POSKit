b8a0c7cf194b48ebf9f218e70d67899baa962bb8
Shelly Tansil <shellytansil93@gmail.com>
2021-12-08 22:55:46 +0700

    feat: membuat widget custom text

---
+++ lib/widgets/custom_text.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CustomText extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  final Color? textColor;
+  final Color? iconColor;
+  final double? textSize;
+  final double? iconSize;
+
+  const CustomText({
+    Key? key,
+    required this.text,
+    required this.icon,
+    this.textColor,
+    this.textSize,
+    this.iconColor,
+    this.iconSize,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: iconSize,
+          color: iconColor,
+        ),
+        const SizedBox(
+          width: 8.0,
+        ),
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: textSize,
+            color: textColor,
+          ),
+          overflow: TextOverflow.ellipsis,
+        ),
+      ],
+    );
+  }
+}
