7070b454fb6a9fd5c52c427fe426710054fd3e31
Shelly Tansil <shellytansil93@gmail.com>
2021-12-09 21:22:51 +0700

    refactor: menerapkan listview history

--- lib/ui/history/history_page.dart
+++ lib/ui/history/history_page.dart
@@ -1,4 +1,5 @@
-import 'package:dicoding_capstone_pos/widgets/empty_widget.dart';
+import 'package:dicoding_capstone_pos/common/styles.dart';
+import 'package:dicoding_capstone_pos/widgets/list_history.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -6,10 +7,25 @@ class HistoryPage extends StatelessWidget {
   static const routeName = '/history';
   static const pageTitle = 'History';
 
+  const HistoryPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: EmptyWidget(),
+      backgroundColor: background,
+      appBar: AppBar(
+        title: const Center(child: Text('History Transaction')),
+        shadowColor: Colors.white54,
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 20.0),
+            child: ListHistory(),
+          );
+        },
+        itemCount: 2,
+      ),
     );
   }
 }
