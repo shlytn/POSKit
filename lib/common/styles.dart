911a6efd0a60f5c25eefe31c17e3af5dcebbe966
Shelly Tansil <shellytansil93@gmail.com>
2021-12-09 21:20:33 +0700

    style: mengganti warna background dan menambahkan beberapa warna

--- lib/common/styles.dart
+++ lib/common/styles.dart
@@ -2,5 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 const Color primaryColor = Color(0xFFFFFFFF);
+const Color onPrimary = Color(0xFF2A3256);
 const secondaryColor = Color(0xFF1A72DD);
-const background = Color(0xFFF2F2F2);
+const onSecondary = Color(0xFFFFFFFF);
+const background = Color(0xFFF7F8FA);
