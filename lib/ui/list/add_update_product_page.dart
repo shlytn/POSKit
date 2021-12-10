240251303eb2ac211e5faca13ad31b5269904abb
Mochamad Fauzan Osama <osamafauzan33@gmail.com>
2021-12-10 11:31:06 +0700

    refactor: menggunakan center title pada appbar

--- lib/ui/list/add_update_product_page.dart
+++ lib/ui/list/add_update_product_page.dart
@@ -24,6 +24,7 @@ class AddUpdateProductPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(title),
       ),
       body: SingleChildScrollView(
