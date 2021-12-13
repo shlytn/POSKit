90b39d0788e70fb0a5f0e954a79efbb140b1e424
Shelly Tansil <shellytansil93@gmail.com>
2021-12-13 13:08:38 +0700

    refactor: mengimplementasikan fungsi untuk mengubah password

--- lib/ui/profile/change_password_page.dart
+++ lib/ui/profile/change_password_page.dart
@@ -1,8 +1,10 @@
+import 'package:dicoding_capstone_pos/provider/auth_provider.dart';
 import 'package:dicoding_capstone_pos/widgets/input_field.dart';
 import 'package:dicoding_capstone_pos/widgets/rounded_button.dart';
 import 'package:dicoding_capstone_pos/widgets/widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ChangePasswordPage extends StatelessWidget {
   static const routeName = '/change_password';
@@ -12,6 +14,10 @@ class ChangePasswordPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var auth = Provider.of<AuthProvider>(context);
+    var currentPassword = '';
+    var newPassword = '';
+
     return Scaffold(
       appBar: AppBar(title: const Text(pageTitle)),
       body: SingleChildScrollView(
@@ -20,14 +26,27 @@ class ChangePasswordPage extends StatelessWidget {
           child: Column(
             children: [
               InputField(
-                  label: 'Current Password', hint: 'Enter Current Password'),
+                label: 'Current Password',
+                hint: 'Enter Current Password',
+                onChanged: (value) => currentPassword = value,
+              ),
               spacing(12.0),
               InputField(
-                  label: 'New Password', hint: 'Must at least 6 character'),
+                label: 'New Password',
+                hint: 'Must at least 6 character',
+                onChanged: (value) => newPassword = value,
+              ),
               spacing(32.0),
               RoundedButton(
                 text: 'Update Password',
-                onClick: () {},
+                onClick: () async {
+                  await auth.changePassword(currentPassword, newPassword);
+
+                  final snackBar = SnackBar(
+                    content: Text(auth.message),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                },
               )
             ],
           ),
