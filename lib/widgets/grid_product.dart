22223b957542ff7f78d844c023249c919d941b03
Shelly Tansil <shellytansil93@gmail.com>
2021-12-13 21:07:29 +0700

    refactor: memindahkan product grid card ke dalam file baru

--- lib/widgets/grid_product.dart
+++ lib/widgets/grid_product.dart
@@ -1,5 +1,4 @@
-import 'package:dicoding_capstone_pos/common/styles.dart';
-import 'package:dicoding_capstone_pos/ui/list/add_update_product_page.dart';
+import 'package:dicoding_capstone_pos/widgets/grid_product_card.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -11,8 +10,6 @@ class GridViewProduct extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String foodName = 'Salad Tuna';
-    double price = 10.99;
     return GridView.builder(
       itemCount: 3,
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -21,80 +18,7 @@ class GridViewProduct extends StatelessWidget {
           mainAxisSpacing: 10,
           childAspectRatio: 10 / 15),
       itemBuilder: (context, index) {
-        return GestureDetector(
-          onDoubleTap: (){
-            Navigator.pushNamed(context, AddUpdateProductPage.routeName,
-                arguments: true);
-          },
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: BoxDecoration(
-                  color: primaryColor, borderRadius: BorderRadius.circular(20)),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        image: const DecorationImage(
-                            image: AssetImage('assets/images/food.jpg'),
-                            fit: BoxFit.cover),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          foodName,
-                          style: const TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(height: 3.0),
-                        const Text(
-                          '(must choose level)',
-                          style: TextStyle(
-                              fontSize: 12, color: CupertinoColors.inactiveGray),
-                        ),
-                        const SizedBox(height: 10.0),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              '\$' + price.toString(),
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: secondaryColor,
-                                  fontSize: 16),
-                            ),
-                            Container(
-                              width: 31.0,
-                              height: 29.0,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  color: secondaryColor),
-                              child: IconButton(
-                                splashColor: Colors.transparent,
-                                iconSize: 20,
-                                padding: const EdgeInsets.all(0),
-                                color: Colors.white,
-                                onPressed: () {},
-                                icon: const Icon(CupertinoIcons.add),
-                              ),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        );
+        return const GridProductCard();
       },
     );
   }
