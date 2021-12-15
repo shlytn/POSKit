ba493c0c111022ec06100b4a3633ebfcfd520ce9
Shelly Tansil <shellytansil93@gmail.com>
2021-12-15 08:34:56 +0700

    refactor: menambahkan StreamProvider untuk mengambil data realtime

--- lib/ui/list/product_list_page.dart
+++ lib/ui/list/product_list_page.dart
@@ -1,10 +1,13 @@
 import 'package:dicoding_capstone_pos/common/styles.dart';
+import 'package:dicoding_capstone_pos/data/models/item.dart';
+import 'package:dicoding_capstone_pos/provider/database_provider.dart';
 import 'package:dicoding_capstone_pos/ui/cart/cart_page.dart';
 import 'package:dicoding_capstone_pos/widgets/cart_button.dart';
 import 'package:dicoding_capstone_pos/widgets/grid_product.dart';
 import 'package:dicoding_capstone_pos/widgets/list_product.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'add_update_product_page.dart';
 
@@ -23,6 +26,8 @@ class _ProductListPageState extends State<ProductListPage> {
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<DatabaseProvider>(context);
+
     return Scaffold(
       backgroundColor: background,
       appBar: AppBar(
@@ -38,79 +43,82 @@ class _ProductListPageState extends State<ProductListPage> {
               icon: const Icon(CupertinoIcons.add),
             ),
           ]),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                flex: 4,
-                child: Container(
-                  height: 56.0,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border:
-                        Border.all(color: CupertinoColors.lightBackgroundGray),
-                  ),
-                  child: const Center(
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: CupertinoColors.inactiveGray),
-                          ),
-                          suffixIcon: Icon(CupertinoIcons.search),
-                          labelText: 'search product',
-                          focusedBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: CupertinoColors.inactiveGray),
+      body: StreamProvider<List<Item>>.value(
+        value: provider.getItems(),
+        initialData: const [],
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  flex: 4,
+                  child: Container(
+                    height: 56.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border:
+                          Border.all(color: CupertinoColors.lightBackgroundGray),
+                    ),
+                    child: const Center(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: CupertinoColors.inactiveGray),
+                            ),
+                            suffixIcon: Icon(CupertinoIcons.search),
+                            labelText: 'search product',
+                            focusedBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: CupertinoColors.inactiveGray),
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  height: 56.0,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border:
-                        Border.all(color: CupertinoColors.lightBackgroundGray),
+                Expanded(
+                  child: Container(
+                    height: 56.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border:
+                          Border.all(color: CupertinoColors.lightBackgroundGray),
+                    ),
+                    child: isListView
+                        ? IconButton(
+                            onPressed: () {
+                              setState(() {
+                                isListView = false;
+                              });
+                            },
+                            icon: const Icon(CupertinoIcons.square_grid_2x2),
+                          )
+                        : IconButton(
+                            onPressed: () {
+                              setState(() {
+                                isListView = true;
+                              });
+                            },
+                            icon: const Icon(CupertinoIcons.list_bullet),
+                          ),
                   ),
-                  child: isListView
-                      ? IconButton(
-                          onPressed: () {
-                            setState(() {
-                              isListView = false;
-                            });
-                          },
-                          icon: const Icon(CupertinoIcons.square_grid_2x2),
-                        )
-                      : IconButton(
-                          onPressed: () {
-                            setState(() {
-                              isListView = true;
-                            });
-                          },
-                          icon: const Icon(CupertinoIcons.list_bullet),
-                        ),
-                ),
-              )
-            ],
-          ),
-          const SizedBox(height: 8.0),
-          Expanded(
-            child:
-                isListView ? const ListViewProduct() : const GridViewProduct(),
-          ),
-          const CartButton(
-            route: CartPage.routeName,
-          ),
-          const SizedBox(height: 8.0)
-        ],
+                )
+              ],
+            ),
+            const SizedBox(height: 8.0),
+            Expanded(
+              child: isListView ? const ListViewProduct() : const GridViewProduct(),
+            ),
+            const CartButton(
+              route: CartPage.routeName,
+            ),
+            const SizedBox(height: 8.0)
+          ],
+        ),
       ),
     );
   }
