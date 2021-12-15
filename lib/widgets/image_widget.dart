e3fc07e8db5bbacd53120ff60155b3e6d9fd97ad
Shelly Tansil <shellytansil93@gmail.com>
2021-12-15 21:32:26 +0700

    refactor: implemen fungsi pickImage pada onTap

--- lib/widgets/image_widget.dart
+++ lib/widgets/image_widget.dart
@@ -1,29 +1,40 @@
+import 'dart:io';
+
 import 'package:dicoding_capstone_pos/common/styles.dart';
+import 'package:dicoding_capstone_pos/provider/image_picker_provider.dart';
+import 'package:dicoding_capstone_pos/widgets/image_bottom_sheet.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ImageWidget extends StatelessWidget {
+  const ImageWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    final image = Provider.of<ImagePickerProvider>(context).image;
+
     return Stack(
       children: [
-        _buildImage(),
-        Positioned(bottom: 0, right: 5, child: _buildCameraButton())
+        _buildImage(image),
+        Positioned(bottom: 0, right: 5, child: _buildCameraButton(context))
       ],
     );
   }
 
-  Widget _buildImage() {
+  Widget _buildImage(File? image) {
     return ClipOval(
-      child: Image.asset(
-        'assets/images/pp.jpg',
-        width: 150.0,
-        height: 150.0,
-      ),
+      child: image != null
+          ? Image.file(image, width: 150.0, height: 150.0, fit: BoxFit.cover)
+          : Image.asset(
+              'assets/images/pp.jpg',
+              width: 150.0,
+              height: 150.0,
+            ),
     );
   }
 
-  Widget _buildCameraButton() {
+  Widget _buildCameraButton(BuildContext context) {
     return ClipOval(
       child: Container(
         decoration: const BoxDecoration(
@@ -31,10 +42,13 @@ class ImageWidget extends StatelessWidget {
         ),
         padding: const EdgeInsets.all(8.0),
         child: InkWell(
-          child: Icon(
+          child: const Icon(
             CupertinoIcons.camera_fill,
             color: primaryColor,
           ),
+          onTap: () {
+            showImageSource(context);
+          },
         ),
       ),
     );
