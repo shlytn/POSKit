0035893838f0ca96959494c3b1e9a971cb40eea4
Shelly Tansil <shellytansil93@gmail.com>
2021-12-16 23:13:12 +0700

    refactor: menambahkan param untuk set enable

--- lib/widgets/rounded_button.dart
+++ lib/widgets/rounded_button.dart
@@ -5,21 +5,23 @@ import 'package:flutter/material.dart';
 class RoundedButton extends StatelessWidget {
   final Function onClick;
   final String text;
+  final bool isEnable;
 
   const RoundedButton({
     Key? key,
     required this.onClick,
     required this.text,
+    this.isEnable = true,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () => onClick(),
+      onPressed: () => isEnable ? onClick() : null,
       child: Text(text),
       style: ElevatedButton.styleFrom(
         minimumSize: const Size.fromHeight(56.0),
-        primary: secondaryColor,
+        primary: isEnable ? secondaryColor : Colors.grey,
         onPrimary: onSecondary,
         textStyle: const TextStyle(fontSize: 16.0),
         shape: RoundedRectangleBorder(
