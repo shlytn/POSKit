55b23e621d333b630b9dab6450c44bcb7ad5f007
Shelly Tansil <shellytansil93@gmail.com>
2021-12-19 22:06:10 +0700

    feat: menambahkan stream provider untuk menampilkan data stream cart

--- lib/ui/cart/cart_page.dart
+++ lib/ui/cart/cart_page.dart
@@ -1,10 +1,13 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:dicoding_capstone_pos/data/models/cart_item.dart';
+import 'package:dicoding_capstone_pos/provider/cart_provider.dart';
 import 'package:dicoding_capstone_pos/ui/cart/order_detail.dart';
 import 'package:dicoding_capstone_pos/widgets/cart_button.dart';
 import 'package:dicoding_capstone_pos/widgets/cart_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatefulWidget {
   static const routeName = '/cart';
@@ -18,23 +21,29 @@ class _CartPageState extends State<CartPage> {
   bool isListView = true;
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<CartProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         elevation: 0,
         title: Text('Cart'),
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 8.0),
-          Expanded(
-            child: ListViewCart(),
-          ),
-          CartButton(
-            route: OrderDetailPage.routeName,
-          ),
-          SizedBox(height: 8.0)
-        ],
+      body: StreamProvider<List<CartItem>>.value(
+        value: provider.getCart(),
+        initialData: [],
+        child: Column(
+          children: [
+            SizedBox(height: 8.0),
+            Expanded(
+              child: ListViewCart(),
+            ),
+            CartButton(
+              route: OrderDetailPage.routeName,
+            ),
+            SizedBox(height: 8.0)
+          ],
+        ),
       ),
     );
   }
