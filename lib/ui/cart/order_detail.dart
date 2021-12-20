f51f76d4a10e3dbfcde6b7652cdbe10cfcc4d8b9
Shelly Tansil <shellytansil93@gmail.com>
2021-12-21 01:26:09 +0700

    refactor: menerapkan stream provider

--- lib/ui/cart/order_detail.dart
+++ lib/ui/cart/order_detail.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:dicoding_capstone_pos/common/styles.dart';
+import 'package:dicoding_capstone_pos/data/models/cart_item.dart';
+import 'package:dicoding_capstone_pos/provider/cart_provider.dart';
 import 'package:dicoding_capstone_pos/widgets/order_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class OrderDetailPage extends StatefulWidget {
   static const routeName = '/order_detail';
@@ -16,8 +19,11 @@ class OrderDetailPage extends StatefulWidget {
 class _OrderDetailPageState extends State<OrderDetailPage> {
   int? defaultChoice;
   final List<String> _choicesList = ['Take Away', 'Delivery', 'Dine In'];
+
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<CartProvider>(context);
+
     return Scaffold(
       backgroundColor: background,
       appBar: AppBar(
@@ -25,139 +31,143 @@ class _OrderDetailPageState extends State<OrderDetailPage> {
         elevation: 0,
         title: Text('Order Details'),
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 56.0,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: CupertinoColors.lightBackgroundGray),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text(
-                    'Customer',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.arrow_forward_ios,
-                      size: 12,
+      body: StreamProvider<List<CartItem>>.value(
+        value: provider.getCart(),
+        initialData: [],
+        child: Column(
+          children: [
+            Container(
+              height: 56.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: CupertinoColors.lightBackgroundGray),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      'Customer',
+                      style: TextStyle(fontSize: 16),
                     ),
                   ),
-                )
-              ],
-            ),
-          ),
-          SizedBox(height: 8.0),
-          Wrap(
-            spacing: 8,
-            children: List.generate(
-              _choicesList.length,
-              (index) {
-                return ChoiceChip(
-                  labelPadding: EdgeInsets.all(2.0),
-                  label: Text(
-                    _choicesList[index],
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  selected: defaultChoice == index,
-                  selectedColor: secondaryColor,
-                  onSelected: (value) {
-                    setState(() {
-                      defaultChoice = value ? index : defaultChoice;
-                    });
-                  },
-                  elevation: 0,
-                );
-              },
-            ),
-          ),
-          SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-            child: Column(
-              children: [
-                OrderList(),
-                Divider(),
-                SizedBox(height: 8.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('Discount', style: TextStyle(fontSize: 16)),
-                    Container(
-                      height: 9,
-                      child: IconButton(
-                        padding: const EdgeInsets.all(0),
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.arrow_forward_ios,
-                          size: 9,
-                        ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.arrow_forward_ios,
+                        size: 12,
                       ),
                     ),
-                  ],
-                ),
-                SizedBox(height: 8.0),
-                Divider(),
-                SizedBox(height: 8.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Subtotal',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                    ),
-                    Text(
-                      '\$10.99',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 8.0),
-                Divider(),
-                SizedBox(height: 8.0),
-                Center(
-                  child: TextButton.icon(
-                    style: TextButton.styleFrom(
-                        primary: CupertinoColors.systemRed),
-                    onPressed: () {},
-                    icon: Icon(CupertinoIcons.trash),
-                    label: Text('Delete Order'),
-                  ),
-                ),
-              ],
+                  )
+                ],
+              ),
             ),
-          ),
-          Spacer(),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                  primary: secondaryColor, onPrimary: Colors.white),
-              child: Container(
-                width: double.infinity,
-                height: 60,
-                child: Center(
-                  child: Text(
-                    'Confirm Order',
-                    style: TextStyle(fontSize: 16),
+            SizedBox(height: 8.0),
+            Wrap(
+              spacing: 8,
+              children: List.generate(
+                _choicesList.length,
+                (index) {
+                  return ChoiceChip(
+                    labelPadding: EdgeInsets.all(2.0),
+                    label: Text(
+                      _choicesList[index],
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    selected: defaultChoice == index,
+                    selectedColor: secondaryColor,
+                    onSelected: (value) {
+                      setState(() {
+                        defaultChoice = value ? index : defaultChoice;
+                      });
+                    },
+                    elevation: 0,
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+              child: Column(
+                children: [
+                  OrderList(),
+                  Divider(),
+                  SizedBox(height: 8.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Discount', style: TextStyle(fontSize: 16)),
+                      Container(
+                        height: 9,
+                        child: IconButton(
+                          padding: const EdgeInsets.all(0),
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.arrow_forward_ios,
+                            size: 9,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 8.0),
+                  Divider(),
+                  SizedBox(height: 8.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Subtotal',
+                        style:
+                            TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      Text(
+                        '\$10.99',
+                        style:
+                            TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 8.0),
+                  Divider(),
+                  SizedBox(height: 8.0),
+                  Center(
+                    child: TextButton.icon(
+                      style: TextButton.styleFrom(
+                          primary: CupertinoColors.systemRed),
+                      onPressed: () {},
+                      icon: Icon(CupertinoIcons.trash),
+                      label: Text('Delete Order'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    primary: secondaryColor, onPrimary: Colors.white),
+                child: Container(
+                  width: double.infinity,
+                  height: 60,
+                  child: Center(
+                    child: Text(
+                      'Confirm Order',
+                      style: TextStyle(fontSize: 16),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
