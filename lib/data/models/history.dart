c9161492c6a6b9a40f192a29ba1348b89720f712
Shelly Tansil <shellytansil93@gmail.com>
2021-12-21 01:41:42 +0700

    refactor: menambahkan tanggal dan waktu

--- lib/data/models/history.dart
+++ lib/data/models/history.dart
@@ -4,17 +4,23 @@ import 'package:dicoding_capstone_pos/data/models/cart_item.dart';
 class History {
   History({
     this.id,
+    required this.date,
+    required this.time,
     required this.items,
     required this.totalItem,
     required this.totalPrice,
   });
 
   String? id;
+  String date;
+  String time;
   List<CartItem> items;
   int totalItem;
   int totalPrice;
 
   factory History.fromJson(Map<String, dynamic> json) => History(
+        date: json["date"],
+        time: json["time"],
         items:
             List<CartItem>.from(json["items"].map((x) => CartItem.fromJson(x))),
         totalItem: json["totalItem"].toInt(),
@@ -23,6 +29,8 @@ class History {
 
   factory History.fromFirebase(DocumentSnapshot doc) => History(
         id: doc.id,
+        date: doc["date"],
+        time: doc["time"],
         items:
             List<CartItem>.from(doc["items"].map((x) => CartItem.fromJson(x))),
         totalItem: doc["totalItem"].toInt(),
@@ -30,7 +38,9 @@ class History {
       );
 
   Map<String, dynamic> toJson() => {
-        "items": items,
+        "date": date,
+        "time": time,
+        "items": List<CartItem>.from(items.map((x) => x.toJson())),
         "totalItem": totalItem,
         "totalPrice": totalPrice,
       };
