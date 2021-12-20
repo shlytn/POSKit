5aaf82ae0ed3e73b37fd4b9d9ab6c13aef98b1b7
Shelly Tansil <shellytansil93@gmail.com>
2021-12-20 17:51:14 +0700

    feat: menambahkan slider pada cart item

--- lib/widgets/cart_list_item.dart
+++ lib/widgets/cart_list_item.dart
@@ -2,6 +2,7 @@ import 'package:dicoding_capstone_pos/common/styles.dart';
 import 'package:dicoding_capstone_pos/data/models/cart_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class CartListItem extends StatelessWidget {
   final CartItem item;
@@ -10,77 +11,92 @@ class CartListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: primaryColor,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
         children: [
-          Expanded(
-            flex: 1,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.asset('assets/images/food.jpg', fit: BoxFit.cover),
-            ),
+          SlidableAction(
+            onPressed: (BuildContext context) {},
+            icon: CupertinoIcons.delete,
+            foregroundColor: Colors.red,
+            backgroundColor: background,
           ),
-          const SizedBox(width: 10.0),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.start,
+        ],
+        extentRatio: 1/5,
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: primaryColor,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset('assets/images/food.jpg', fit: BoxFit.cover),
+              ),
+            ),
+            const SizedBox(width: 10.0),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      item.item.name,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 30.0),
+                    Text(
+                      item.item.sellingPrice.toString(),
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: secondaryColor),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 32.0,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: secondaryColor),
+              margin: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
                 children: [
-                  Text(
-                    item.item.name,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  IconButton(
+                    iconSize: 16,
+                    padding: const EdgeInsets.all(0),
+                    color: Colors.white,
+                    onPressed: () {},
+                    icon: const Icon(CupertinoIcons.minus),
                   ),
-                  const SizedBox(height: 30.0),
                   Text(
-                    item.item.sellingPrice.toString(),
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        color: secondaryColor),
-                  )
+                    item.quantity.toString(),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  IconButton(
+                    splashColor: Colors.red,
+                    iconSize: 16,
+                    padding: const EdgeInsets.all(0),
+                    color: Colors.white,
+                    onPressed: () {},
+                    icon: const Icon(CupertinoIcons.add),
+                  ),
                 ],
               ),
             ),
-          ),
-          Container(
-            height: 32.0,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10), color: secondaryColor),
-            margin: const EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  iconSize: 16,
-                  padding: const EdgeInsets.all(0),
-                  color: Colors.white,
-                  onPressed: () {},
-                  icon: const Icon(CupertinoIcons.minus),
-                ),
-                Text(
-                  item.quantity.toString(),
-                  style: const TextStyle(color: Colors.white),
-                ),
-                IconButton(
-                  splashColor: Colors.red,
-                  iconSize: 16,
-                  padding: const EdgeInsets.all(0),
-                  color: Colors.white,
-                  onPressed: () {},
-                  icon: const Icon(CupertinoIcons.add),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
