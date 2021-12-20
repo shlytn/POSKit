1440b2eca8ff507ff202b936360b439906ae7855
Shelly Tansil <shellytansil93@gmail.com>
2021-12-21 00:41:46 +0700

    refactor: menerapkan item

--- lib/widgets/order_list.dart
+++ lib/widgets/order_list.dart
@@ -1,17 +1,21 @@
 import 'package:dicoding_capstone_pos/common/styles.dart';
+import 'package:dicoding_capstone_pos/data/models/cart_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class OrderList extends StatelessWidget {
   const OrderList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final items = Provider.of<List<CartItem>>(context);
+
     return ListView.separated(
       shrinkWrap: true,
-      separatorBuilder: (context, index) => Divider(),
-      itemCount: 1,
-      itemBuilder: (context, snapshot) {
+      separatorBuilder: (context, index) => const Divider(),
+      itemCount: items.length,
+      itemBuilder: (context, index) {
         return Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
@@ -23,17 +27,17 @@ class OrderList extends StatelessWidget {
                   color: secondaryColor),
               child: Center(
                 child: Text(
-                  '1',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
+                  items[index].quantity.toString(),
+                  style: const TextStyle(color: Colors.white, fontSize: 16),
                 ),
               ),
             ),
-            SizedBox(width: 25.0),
-            Text('Tuna Salad'),
-            Spacer(),
+            const SizedBox(width: 25.0),
+            Text(items[index].item.name),
+            const Spacer(),
             Align(
               alignment: Alignment.centerRight,
-              child: Text('\$10.99'),
+              child: Text(items[index].total.toString()),
             )
           ],
         );
