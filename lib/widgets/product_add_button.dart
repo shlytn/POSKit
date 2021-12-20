ec2028e60d2d2821c640ff8d1ce54966ef01f821
Shelly Tansil <shellytansil93@gmail.com>
2021-12-20 11:44:08 +0700

    feat: menerapkan provider untuk add item cart

--- lib/widgets/product_add_button.dart
+++ lib/widgets/product_add_button.dart
@@ -1,8 +1,10 @@
 import 'package:dicoding_capstone_pos/data/models/cart_item.dart';
 import 'package:dicoding_capstone_pos/data/models/item.dart';
+import 'package:dicoding_capstone_pos/provider/cart_provider.dart';
 import 'package:dicoding_capstone_pos/widgets/widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ProductAddButton extends StatelessWidget {
   final Item item;
@@ -11,12 +13,19 @@ class ProductAddButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<CartProvider>(context);
+
     return IconButton(
         splashColor: Colors.transparent,
         iconSize: 24,
         padding: const EdgeInsets.all(0),
         color: Colors.white,
-        onPressed: () {},
+        onPressed: () async {
+          final cartItem = CartItem(item: item, quantity: 1, total: item.sellingPrice);
+          await provider.addCart(cartItem);
+
+          showMessageSnackBar(context, provider.message);
+        },
         icon: const Icon(CupertinoIcons.add),
     );
   }
