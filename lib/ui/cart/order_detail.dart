9c83ff60f26cc0e3951650ace787219973d26492
Shelly Tansil <shellytansil93@gmail.com>
2021-12-21 01:27:03 +0700

    refactor: menghapus baris code yang tidak terpakai

--- lib/ui/cart/order_detail.dart
+++ lib/ui/cart/order_detail.dart
@@ -171,42 +171,7 @@ class _OrderDetailPageState extends State<OrderDetailPage> {
       ),
     );
   }
-
-  Widget _buildOrderList() {
-    return ListView.separated(
-      shrinkWrap: true,
-      separatorBuilder: (context, index) => Divider(),
-      itemCount: 1,
-      itemBuilder: (context, snapshot) {
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              height: 34,
-              width: 34,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: secondaryColor),
-              child: Center(
-                child: Text(
-                  '1',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                ),
-              ),
-            ),
-            SizedBox(width: 25.0),
-            Text('Tuna Salad'),
-            Spacer(),
-            Align(
-              alignment: Alignment.centerRight,
-              child: Text('\$10.99'),
-            )
-          ],
-        );
-      },
-    );
-  }
-
+  
   @override
   void initState() {
     super.initState();
