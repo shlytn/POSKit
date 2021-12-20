8284ec8436a7fe2d328104bd631b591f9bb2cd80
Shelly Tansil <shellytansil93@gmail.com>
2021-12-20 23:21:22 +0700

    refactor: menyesuaikan state halaman dengan state data

--- lib/widgets/product_grid.dart
+++ lib/widgets/product_grid.dart
@@ -1,4 +1,6 @@
 import 'package:dicoding_capstone_pos/data/models/item.dart';
+import 'package:dicoding_capstone_pos/provider/database_provider.dart';
+import 'package:dicoding_capstone_pos/utils/result_state.dart';
 import 'package:dicoding_capstone_pos/widgets/empty_widget.dart';
 import 'package:dicoding_capstone_pos/widgets/product_grid_card.dart';
 
@@ -15,20 +17,28 @@ class GridViewProduct extends StatelessWidget {
   Widget build(BuildContext context) {
     final items = Provider.of<List<Item>>(context);
 
-    if(items.isEmpty){
-      return const Center(child: EmptyWidget());
-    }
+    return Consumer<DatabaseProvider>(
+      builder: (context, provider, _) {
+        if (provider.state == ResultState.error){
+          return const Center(child: Text("Ups, something went wrong!"));
+        } else if (provider.state == ResultState.noData){
+          return const Center(child: EmptyWidget());
+        } else if (provider.state == ResultState.hasData) {
+          return GridView.builder(
+            itemCount: items.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                childAspectRatio: 10 / 15),
+            itemBuilder: (context, index) {
+              return GridProductCard(item: items[index],);
+            },
+          );
+        }
 
-    return GridView.builder(
-      itemCount: items.length,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          childAspectRatio: 10 / 15),
-      itemBuilder: (context, index) {
-        return GridProductCard(item: items[index],);
-      },
+        return const CircularProgressIndicator();
+      }
     );
   }
 }
--- lib/widgets/product_list.dart
+++ lib/widgets/product_list.dart
@@ -2,6 +2,8 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
 import 'package:dicoding_capstone_pos/data/models/item.dart';
+import 'package:dicoding_capstone_pos/provider/database_provider.dart';
+import 'package:dicoding_capstone_pos/utils/result_state.dart';
 import 'package:dicoding_capstone_pos/widgets/empty_widget.dart';
 import 'package:dicoding_capstone_pos/widgets/product_list_card.dart';
 import 'package:flutter/material.dart';
@@ -17,18 +19,25 @@ class ListViewProduct extends StatelessWidget {
   Widget build(BuildContext context) {
     final items = Provider.of<List<Item>>(context);
 
-    if(items.isEmpty){
-      return const Center(child: EmptyWidget());
-    }
-
-    return ListView.separated(
-      separatorBuilder: (context, index) {
-        return Divider();
-      },
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        return CardProduct(item: items[index],);
-      },
+    return Consumer<DatabaseProvider>(
+      builder: (context, provider, _) {
+        if (provider.state == ResultState.error){
+          return const Center(child: Text("Ups, something went wrong!"));
+        } else if (provider.state == ResultState.noData){
+          return const Center(child: EmptyWidget());
+        } else if (provider.state == ResultState.hasData) {
+          return ListView.separated(
+            separatorBuilder: (context, index) {
+              return Divider();
+            },
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              return CardProduct(item: items[index]);
+            },
+          );
+        }
+        return const CircularProgressIndicator();
+      }
     );
   }
 }
