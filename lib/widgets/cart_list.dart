73b800d2fc9cb7c4ebf908c94c83fc3457262ae8
Shelly Tansil <shellytansil93@gmail.com>
2021-12-20 23:51:31 +0700

    refactor: menambahkan check state, menyeseuaikan state halaman dengan state data

--- lib/widgets/cart_list.dart
+++ lib/widgets/cart_list.dart
@@ -2,7 +2,9 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
 import 'package:dicoding_capstone_pos/data/models/cart_item.dart';
+import 'package:dicoding_capstone_pos/provider/cart_provider.dart';
 import 'package:dicoding_capstone_pos/widgets/cart_list_item.dart';
+import 'package:dicoding_capstone_pos/widgets/check_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
@@ -16,17 +18,23 @@ class ListViewCart extends StatelessWidget {
   Widget build(BuildContext context) {
     final items = Provider.of<List<CartItem>>(context);
 
-    return ListView.separated(
-      separatorBuilder: (context, index) {
-        return Divider();
-      },
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 4.0),
-          child: CartListItem(item: items[index]),
-        );
-      },
-    );
+    return Consumer<CartProvider>(builder: (context, provider, _) {
+      return CheckState(
+        provider: provider,
+        child: ListView.separated(
+          separatorBuilder: (context, index) {
+            return Divider();
+          },
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 18.0, vertical: 4.0),
+              child: CartListItem(item: items[index]),
+            );
+          },
+        ),
+      );
+    });
   }
 }
