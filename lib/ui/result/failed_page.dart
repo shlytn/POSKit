333acd6fca4430862e399dd50a30d166cf9cd576
Mochamad Fauzan Osama <osamafauzan33@gmail.com>
2021-12-21 19:22:27 +0700

    feat: membuat halaman failed result

---
+++ lib/ui/result/failed_page.dart
@@ -0,0 +1,84 @@
+import 'package:dicoding_capstone_pos/common/styles.dart';
+import 'package:dicoding_capstone_pos/ui/home/home_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FailedPage extends StatelessWidget {
+  static const routeName = '/failed';
+  const FailedPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: error,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: primaryColor,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      CupertinoIcons.xmark_circle_fill,
+                      color: CupertinoColors.destructiveRed,
+                      size: 140,
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      'Transaction Failed',
+                      style: TextStyle(
+                          color: error,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22),
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      'NOTE: Do not forget to give smile to customers.',
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 8.0),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Spacer(),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, HomePage.routeName);
+              },
+              style: ElevatedButton.styleFrom(
+                  primary: primaryColor, onPrimary: error),
+              child: Container(
+                width: double.infinity,
+                height: 60,
+                child: Center(
+                  child: Text(
+                    'NEXT ORDER',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
