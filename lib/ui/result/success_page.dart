976f52ff125832d5ae8ef8fdce5b1e443ccfe4d4
Mochamad Fauzan Osama <osamafauzan33@gmail.com>
2021-12-23 02:55:16 +0700

    feat: menampilkan total harga

--- lib/ui/result/success_page.dart
+++ lib/ui/result/success_page.dart
@@ -1,7 +1,11 @@
 import 'package:dicoding_capstone_pos/common/styles.dart';
+import 'package:dicoding_capstone_pos/provider/cart_provider.dart';
 import 'package:dicoding_capstone_pos/ui/home/home_page.dart';
+import 'package:dicoding_capstone_pos/widgets/check_state.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
 
 class SuccessPage extends StatelessWidget {
   static const routeName = '/success';
@@ -9,109 +13,119 @@ class SuccessPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: secondaryColor,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: primaryColor,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Icon(
-                      CupertinoIcons.check_mark_circled_solid,
-                      color: CupertinoColors.systemGreen,
-                      size: 140,
+    final formatCurrency = NumberFormat("#,##0", "en_US");
+    return Consumer<CartProvider>(
+      builder: (context, provider, _) {
+        return CheckState(
+          provider: provider,
+          child: Scaffold(
+            backgroundColor: secondaryColor,
+            appBar: AppBar(
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+            ),
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: primaryColor,
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    SizedBox(height: 8.0),
-                    Text(
-                      'Transaction Successful',
-                      style: TextStyle(
-                          color: secondaryColor,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 22),
-                    ),
-                    SizedBox(height: 8.0),
-                    Text(
-                      'NOTE: Do not forget to give smile to customers.',
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(height: 8.0),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: secondaryColor,
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 8.0, horizontal: 16.0),
-                          child: Column(
-                            children: [
-                              Text(
-                                'Methode Payment: CASH',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: primaryColor),
-                              ),
-                              Divider(
-                                height: 16,
-                                color: primaryColor,
-                              ),
-                              Text(
-                                'Money Changes:',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: primaryColor),
-                              )
-                            ],
+                    child: Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            CupertinoIcons.check_mark_circled_solid,
+                            color: CupertinoColors.systemGreen,
+                            size: 140,
                           ),
+                          SizedBox(height: 8.0),
+                          Text(
+                            'Transaction Successful',
+                            style: TextStyle(
+                                color: secondaryColor,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 22),
+                          ),
+                          SizedBox(height: 8.0),
+                          Text(
+                            'NOTE: Do not forget to give smile to customers.',
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: 8.0),
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: secondaryColor,
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 8.0, horizontal: 16.0),
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      'Methode Payment: CASH',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: primaryColor),
+                                    ),
+                                    Divider(
+                                      height: 16,
+                                      color: primaryColor,
+                                    ),
+                                    Text(
+                                      'Subtotal: Rp ${formatCurrency.format(provider.totalPrice)}',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: primaryColor),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(
+                          context, HomePage.routeName);
+                    },
+                    style: ElevatedButton.styleFrom(
+                        primary: primaryColor, onPrimary: secondaryColor),
+                    child: Container(
+                      width: double.infinity,
+                      height: 60,
+                      child: Center(
+                        child: Text(
+                          'NEXT ORDER',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
                         ),
                       ),
                     ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Spacer(),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, HomePage.routeName);
-              },
-              style: ElevatedButton.styleFrom(
-                  primary: primaryColor, onPrimary: secondaryColor),
-              child: Container(
-                width: double.infinity,
-                height: 60,
-                child: Center(
-                  child: Text(
-                    'NEXT ORDER',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                   ),
                 ),
-              ),
+              ],
             ),
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
