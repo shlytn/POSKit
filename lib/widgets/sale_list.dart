8982ed662534c7b29c468dada55f85eaf2e4499c
Shelly Tansil <shellytansil93@gmail.com>
2021-12-24 14:52:03 +0700

    refactor: menambahkan tampilan ketika data kosong

--- lib/widgets/sale_list.dart
+++ lib/widgets/sale_list.dart
@@ -1,4 +1,6 @@
 import 'package:dicoding_capstone_pos/data/models/item.dart';
+import 'package:dicoding_capstone_pos/ui/list/add_update_product_page.dart';
+import 'package:dicoding_capstone_pos/widgets/rounded_button.dart';
 import 'package:dicoding_capstone_pos/widgets/sale_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
@@ -11,6 +13,18 @@ class SaleList extends StatelessWidget {
     var items = List.from(Provider.of<List<Item>>(context));
 
     items.sort((b, a) => a.sold!.compareTo(b.sold!));
+
+    if (items.isEmpty) {
+      return Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          child: RoundedButton(
+            text: 'Start a Transaction',
+            onClick: () => Navigator.pushNamed(context, AddUpdateProductPage.routeName),
+          ),
+        ),
+      );
+    }
 
     return ListView.builder(
       itemBuilder: (context, index) {
