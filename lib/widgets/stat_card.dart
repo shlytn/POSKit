9e280a7b33cfa55f4b90c2923f08fbe17540e902
Shelly Tansil <shellytansil93@gmail.com>
2021-12-24 11:07:08 +0700

    feat: menampilkan total transaksi dan total pendapatan

--- lib/widgets/stat_card.dart
+++ lib/widgets/stat_card.dart
@@ -3,27 +3,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class StatCard extends StatelessWidget {
-  final bool isProfit;
-  final bool isIncrease;
-  final String percentage;
+  final bool isIncome;
   final String total;
 
   const StatCard({
     Key? key,
-    required this.isProfit,
-    required this.isIncrease,
-    required this.percentage,
+    required this.isIncome,
     required this.total,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    var color = Color(isProfit ? 0XFFECF5EE : 0XFFCCF0FB);
-    var iconColor = Color(isProfit
-        ? isIncrease
-            ? 0XFF458F5A
-            : 0XFFBF4949
-        : 0xFF00B2EB);
+    var color = Color(isIncome ? 0XFF458F5A : 0xFF00B2EB);
 
     return Container(
       decoration: BoxDecoration(
@@ -32,54 +23,24 @@ class StatCard extends StatelessWidget {
       ),
       padding: const EdgeInsets.all(8.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            decoration: BoxDecoration(
-              color: color,
-              borderRadius: BorderRadius.circular(12.0),
-            ),
-            padding: const EdgeInsets.all(2.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Icon(
-                  isIncrease
-                      ? CupertinoIcons.arrow_up_square_fill
-                      : CupertinoIcons.arrow_down_square_fill,
-                  color: iconColor,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: RichText(
-                    text: TextSpan(
-                      text: isIncrease ? "+" : "-",
-                      children: [
-                        TextSpan(text: percentage),
-                      ],
-                      style: TextStyle(color: iconColor),
-                    ),
-                  ),
-                )
-              ],
-            ),
+          Text(
+            isIncome ? "Total Income" : "Total Transaction",
+            style: TextStyle(color: color),
           ),
           Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: FittedBox(
-                fit: BoxFit.fitWidth,
-                child: Text(
-                  total,
-                  style: const TextStyle(
-                    fontSize: 22.0,
-                    fontWeight: FontWeight.bold,
-                  ),
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: FittedBox(
+              fit: BoxFit.fitWidth,
+              child: Text(
+                total,
+                style: const TextStyle(
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold,
                 ),
-              )),
-          Text(
-            isProfit ? "Total Profit" : "Total Transaction",
-            style: const TextStyle(color: Color(0XFF737D93)),
+              ),
+            ),
           ),
         ],
       ),
--- lib/widgets/stats_widget.dart
+++ lib/widgets/stats_widget.dart
@@ -1,35 +1,42 @@
+import 'package:dicoding_capstone_pos/data/models/history.dart';
+import 'package:dicoding_capstone_pos/provider/history_provider.dart';
 import 'package:dicoding_capstone_pos/widgets/stat_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 class StatsWidget extends StatelessWidget {
   const StatsWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<HistoryProvider>(context);
     final formatCurrency = NumberFormat("#,##0.00", "en_US");
     var width = MediaQuery.of(context).size.width;
 
-    return Row(
-      children: [
-        Expanded(
-          child: StatCard(
-              isProfit: true,
-              isIncrease: true,
-              percentage: "8.00%",
-              total: "Rp. ${formatCurrency.format(50000)}"),
-        ),
-        SizedBox(
-          width: width * 0.025,
-        ),
-        const Expanded(
-          child: StatCard(
-              isProfit: false,
-              isIncrease: true,
-              percentage: "2.13%",
-              total: "150"),
-        ),
-      ],
+    return StreamBuilder<List<History>>(
+      stream: provider.getHistory(),
+      builder: (context, snapshot) {
+        var totalIncome = snapshot.hasData ? provider.totalIncome : 0;
+        var totalTransaction = snapshot.hasData ? provider.totalTransaction : 0;
+        return Row(
+          children: [
+            Expanded(
+              child: StatCard(
+                  isIncome: true,
+                  total: "Rp ${formatCurrency.format(totalIncome)}"),
+            ),
+            SizedBox(
+              width: width * 0.025,
+            ),
+            Expanded(
+              child: StatCard(
+                  isIncome: false,
+                  total: "$totalTransaction"),
+            ),
+          ],
+        );
+      },
     );
   }
 }
